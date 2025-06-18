7484a69bf0ffc3c3cd75e597835157772ff49cd9
Lerato Nkhatho <lerato.nkhatho@gmail.com>
2025-06-18 20:33:17 +0200

    Act 3: Remove the Zero in the Display

--- script.js
+++ script.js
@@ -10,8 +10,30 @@ let justCalculated = false;
 
 function appendToDisplay(value) {
     console.log('Button pressed:', value);
+    // If a user just calculated, remove when they calculate again
+    let currentValue = display.value;
 
-    alert('You pressed: ' + value);
+    if (justCalculated && !isNaN(value)) {
+        display.value = value;
+        justCalculated = false;
+        return;  //End the function
+    }
+
+    // If the current display shows 0 and user enters a number, replace the 0 with the number pressed
+    if (currentValue === '0' && !isNaN(value)) {
+        display.value = value;
+    } else if (currentValue === '0' && value === '.'){
+        display.value = currentValue + value;
+    } else {
+        display.value = currentValue + value;
+    }
+    // "else if" above = If the current display shows 0 and the user enters decimal, keep the 0
+
+    // Reset the justCalculated flag when the user starts typing
+    justCalculated = false;
+
+    console.log('Display updated to: ', display.value);
+
 }
 
 function clearDisplay() {
@@ -22,6 +44,16 @@ function clearDisplay() {
 
 function deleteLast() {
     console.log('Delete button pressed.');
+
+    let currentValue = display.value;
+
+    // If there is only one character or its 0, reset to 0
+
+    if (currentValue.length <= 1 || currentValue === '0') {
+        display.value = '0';
+    } else {
+        display.value = currentValue.slice(0, -1);  // If not, remove the last character
+    }
 
     alert('Backspace button was clicked');
 }
