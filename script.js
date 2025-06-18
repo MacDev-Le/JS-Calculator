bb9f20d103fb73ad85d6deba47731b4e9c310d2c
Lerato Nkhatho <lerato.nkhatho@gmail.com>
2025-06-18 20:06:32 +0200

    Act 2: Event Handlers

---
+++ script.js
@@ -0,0 +1,46 @@
+
+// Reference display element
+const display = document.getElementById('display');
+
+// Track if we have performed a calculation
+
+let justCalculated = false;
+
+// Function to append to display
+
+function appendToDisplay(value) {
+    console.log('Button pressed:', value);
+
+    alert('You pressed: ' + value);
+}
+
+function clearDisplay() {
+    console.log('Clear button pressed.');
+
+    alert('Clear button was clicked');
+}
+
+function deleteLast() {
+    console.log('Delete button pressed.');
+
+    alert('Backspace button was clicked');
+}
+
+function calculate() {
+    console.log('Equals button pressed.');
+
+    alert('Equals button was clicked');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Calculator loaded successfully');
+    console.log('Display elemt', display);
+
+    // Test if we can access display
+
+    if (display) {
+        console.log("Current display value: ", display.value);
+    } else {
+        console.log('Display element not found');
+    }
+})
